b3955c32e3a85c81b2c9e031dcb8c4aff63e976f
five <niip42@gmail.com>
2025-06-23 19:04:49 +0900

    chores

--- web/server.js
+++ web/server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 const app = require('./app');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 
 // Database connection
 const databaseUrl = process.env.DATABASE_URL;
